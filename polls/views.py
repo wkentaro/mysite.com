f2e2d57467ada21e298ac86f01dac6528cecb0b8
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-01-11 15:08:09 +0900

    complete the tutorial03 at https://docs.djangoproject.com/en/1.7/intro/tutorial03/

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 # from django.template import RequestContext, loader
 
@@ -17,10 +17,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except:
-        raise Http404
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except:
+    #     raise Http404
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
